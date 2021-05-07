7b92bc5e91924af7f013219d63ee18e1a64b2be4
Dilara Derinpınar <dilaraderinpinar1@gmail.com>
2021-05-07 22:58:30 +0300

    4.HAFTA YOUTUBE ÖDEVİ

---
+++ interfaces/src/interfaces/Main.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//4.HAFTA INTERFACE ÖDEVÝ
+			ICustomerDal customerDal=new MySqlCustomerDal();
+			
+	}
+
+}
---
+++ interfaces/src/interfaces/MySqlCustomerDal.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public class MySqlCustomerDal implements ICustomerDal {
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		System.out.println("mysql e eklendi");
+	}
+
+}
